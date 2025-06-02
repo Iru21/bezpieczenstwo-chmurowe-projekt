c97e6a0cb7175405eb63845919aed24672ec935c
Mateusz Samsel <m.samsel.113@studms.ug.edu.pl>
2025-06-02 15:58:56 +0200

    Cleanup backend and create a basic server

--- backend/lib/index.ts
+++ backend/lib/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world")
+import express from "express"
+import { createServer } from "http"
+
+const app = express()
+const server = createServer(app)
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!")
+})
+
+const PORT = 3000
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
